660fa6385af100d6b38799b00f4a82f95b07e4ca
David Muto <david.muto@gmail.com>
2013-09-04 09:17:47 -0400

    creating shared/global assembly info

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wordpress.Xml.Rpc")]
+[assembly: AssemblyDescription("A .NET XMLRPC Client for Wordpress")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shaw Media, Inc")]
+[assembly: AssemblyProduct("Wordpress.Xml.Rpc")]
+[assembly: AssemblyCopyright("Copyright © Shaw Media, Inc 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(GlobalAssemblyInfo.Version + ".0")]
+[assembly: AssemblyInformationalVersion(GlobalAssemblyInfo.Version)]
+[assembly: AssemblyFileVersion(GlobalAssemblyInfo.Version + ".0")]
+
+class GlobalAssemblyInfo
+{
+    public const string Version = "0.1.0";
+}
