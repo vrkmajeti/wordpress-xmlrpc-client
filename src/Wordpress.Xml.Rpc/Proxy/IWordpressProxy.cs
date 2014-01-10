38e3b8eda676f8705fe8f4e68912b4504ccfa9c0
Vijay Majeti <vijaymajeti@yahoo.com>
2014-01-10 10:33:26 -0500

    correct the xmlrpc method return type for getauthors/getposts as a standard array.

--- src/Wordpress.Xml.Rpc/Proxy/IWordpressProxy.cs
+++ src/Wordpress.Xml.Rpc/Proxy/IWordpressProxy.cs
@@ -10,12 +10,12 @@ namespace Wordpress.Xml.Rpc
     public interface IWordpressProxy : IXmlRpcProxy
     {
         [XmlRpcMethod("wp.getPosts")]
-        IEnumerable<Post> GetPosts(int blogId, string username, string password, PostFilterProxy? filter);
+        Post[] GetPosts(int blogId, string username, string password, PostFilterProxy? filter);
 
         [XmlRpcMethod("wp.getPost")]
         Post GetPost(int blogId, string username, string password, int postId);
 
         [XmlRpcMethod("wp.getAuthors")]
-        IEnumerable<Author> GetAuthors(int blogId, string username, string password);
+        Author[] GetAuthors(int blogId, string username, string password);
     }
 }
